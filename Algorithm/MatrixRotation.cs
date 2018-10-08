40da7ba94e38e991b590cada6e9a7959cc93af43
deepaksingh008 <dpksngh480@gmail.com>
2018-10-08 13:00:14 +0530

    Complete Code

--- Algorithm/MatrixRotation.cs
+++ Algorithm/MatrixRotation.cs
@@ -13,7 +13,7 @@ namespace Algorithm
             Console.WriteLine("Enter no of column");
             int column = int.Parse(Console.ReadLine());
 
-            int[,] vs = new int[row,column];
+            int[,] vs = new int[row, column];
 
 
             for (int i = 0; i < row; i++)
@@ -24,7 +24,7 @@ namespace Algorithm
                 for (int j = 0; j < column; j++)
                 {
                     int columnvalue = int.Parse(Console.ReadLine());
-                    vs[i,j] = columnvalue;
+                    vs[i, j] = columnvalue;
 
                 }
 
@@ -48,41 +48,64 @@ namespace Algorithm
         public void RoatateMatrix()
         {
             int[,] vs = CreateMatrix();
-            int[,] finalMatrix = vs;
+
+            int[,] finalMatrix = vs.Clone() as int[,];
 
             while (true)
             {
                 int columnCount = 0;
                 int rowcount = 0;
-                columnCount = finalMatrix.GetLength(1);
-                rowcount = finalMatrix.GetLength(0);
+                int columnvalue = 0;
+                int rowvalue = 0;
+
+                columnCount = vs.GetLength(1);
+                rowcount = vs.GetLength(0);
+
 
 
                 //For Right Rotation
 
-                for (int i = 0; i < columnCount-1; i++)
+                for (int i = 0; i < rowcount; i++)
                 {
 
+
+                    for (int j = 1; j < columnCount; j++)
+                    {
+                        finalMatrix[i, j] = vs[i, j - 1];
+                        columnvalue = j;
+
+                    }
+                    break;
                 }
                 //For Down Rotation
-                for (int i = 0; i < rowcount-1; i++)
+                for (int i = 0; i < rowcount - 1; i++)
                 {
-
+                    finalMatrix[i + 1, columnvalue] = vs[i, columnvalue];
+                    rowvalue = i + 1;
                 }
                 //For Left Rotation
-                for (int i = 0; i < columnCount-1; i++)
+                for (int i = columnCount - 1; i > 0; i--)
                 {
-
+                    finalMatrix[rowvalue, i - 1] = vs[rowvalue, i];
+                    columnvalue = i - 1;
                 }
                 //For Up Rotation
-                for (int i = 0; i < rowcount-1; i++)
+                for (int i = rowvalue; i > 0; i--)
                 {
-
+                    finalMatrix[i - 1, columnvalue] = vs[i, columnvalue];
                 }
+                break;
 
 
-
-
+            }
+            Console.WriteLine("After Rotation");
+            for (int i = 0; i < finalMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < finalMatrix.GetLength(1); j++)
+                {
+                    Console.Write(string.Format("{0}\t", finalMatrix[i, j]));
+                }
+                Console.WriteLine();
             }
         }
     }
