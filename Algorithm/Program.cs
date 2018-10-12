2141aabae90f707deda5b19094424b07e0075a40
deepaksingh008 <dpksngh480@gmail.com>
2018-10-12 11:24:38 +0530

    90 degree rotation

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -10,65 +10,9 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-
-            Program matrix90DegreeRotation = new Program();
-            matrix90DegreeRotation.HashTheHostImplementation();
-
-
-
             Console.ReadKey();
         }
-        public void HashTheHostImplementation()
-        {
-            int input = 0;
-            input = int.Parse(Console.ReadLine());
-            int[,] vs = new int[input, input];
-
-            for (int i = 0; i < input; i++)
-            {
-                for (int j = 0; j < input; j++)
-                {
-
-                    if (i == j)
-                    {
-                        vs[i, j] = -1;
-                    }
-                    else
-                    {
-                        vs[i, j] = 0;
-                    }
-
-                }
-            }
-
-
-            for (int i = 0; i < input; i++)
-            {
-                string[] inputstring = Console.ReadLine().Split(' ');
-
-                for (int s = 1; s < inputstring.Length; s++)
-                {
-                    int indexvalue = Convert.ToInt32(inputstring[s]);
-                    vs[i, indexvalue - 1] = 1;
-                }
-            }
-
-            int finalcount = 0;
-            for (int i = 0; i < vs.GetLength(0); i++)
-            {
-                for (int j = 0; j < vs.GetLength(1); j++)
-                {
-                    if (vs[i, j] == 0)
-                    {
-                        finalcount++;
-                    }
-
-                }
-            }
-
-            Console.WriteLine(finalcount);
-
-        }
+        
 
         public static void Factorcalculation()
         {
